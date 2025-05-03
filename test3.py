efeb8ebfa7a6db2b2d6db154bcd64cc1901c8d8a
ANSHUL GOLA <anshulgola5339@gmail.com>
2025-05-03 23:22:08 +0530

    Add files via upload

---
+++ test3.py
@@ -0,0 +1,5 @@
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+img=cv2.imread(r"C:\Users\pc\Downloads\wallpaperbetter.com_1920x1080(1).jpg",cv2.IMREAD_GRAYSCALE)
+cv2.imshow("image:- ",img)
